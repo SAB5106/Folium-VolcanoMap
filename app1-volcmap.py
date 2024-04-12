fa7bd63d7d7a731d6b6de06124d9e514f30d4b70
SAB5106 <108255581+SAB5106@users.noreply.github.com>
2024-04-12 12:06:37 +0100

    Update app1-volcmap.py
    Added additional comments to explain code

--- app1-volcmap.py
+++ app1-volcmap.py
@@ -1,24 +1,24 @@
 import folium
 import pandas as pd
 
-# Initialise volcano data
+# Read in volcano data
 data = pd.read_csv("./volcanoes.csv")
-# Initialise pandas variables
+# Variables for map markers
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
-# Initialise map
+# Initialise map; the focus starts around the US MidWest
 map = folium.Map(location=[41.59901965178514, -109.6281607566247],zoom_start=4.5)
 
-# Function for icon colour
+# Function for map marker colour
 def iconColour(elevation):
     if elevation > 3000: return "red"
     elif elevation > 1500: return "orange"
     elif elevation > 1000: return "green"
     else: return "lightgreen"
 
-# Add map markers
+# Iterate through volcano list; create map marker then add to marker group
 markerGroup = folium.FeatureGroup(name="Volcano Map")
 for lt,ln,el,nm in zip(lat,lon,elev,name):
     markerGroup.add_child(folium.CircleMarker(
@@ -31,6 +31,7 @@ for lt,ln,el,nm in zip(lat,lon,elev,name):
         color=iconColour(el)
     ))
 
-# Output to HTML file
+# Add contents of marker group to map
 map.add_child(markerGroup)
+# Save map HTML file
 map.save("./VolcanoMap.html")
